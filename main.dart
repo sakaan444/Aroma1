dc6947b2a345fc78a44ef2cf1c5f9305521c7af1
sakaan444 <sakaan444@gmail.com>
2025-06-28 21:57:20 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aroma',
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: const WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://ar-aroma.com"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebViewWidget(controller: _controller),
+    );
+  }
+}
